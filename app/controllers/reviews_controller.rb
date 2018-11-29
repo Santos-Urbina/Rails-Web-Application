667fa183adcf358a17e40b99a15bd0c093b6262f
Brandon Earnhardt <bmearnhardt@gmail.com>
2018-11-29 16:14:35 +0000

    More CSS cleaning

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,13 @@
 class ReviewsController < ApplicationController
-    def create
-        @restaurant = Restaurant.find(params[:restaurant_id])
-        @review = @restaurant.reviews.create(review_params)
-        redirect_to restaurant_path(@restaurant)
-    end
-    
-    private
-        def review_params
-            params.require(:review).permit(:reviewer, :body)
-        end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.create(review_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private def review_params
+    params.require(:review).permit(:reviewer, :body)
+  end
+
 end
