78a68fc526621ba7336da026434f3be07403e166
Brandon Earnhardt <bmearnhardt@gmail.com>
2018-11-30 10:35:03 -0500

    Finished the CSS, organized HTML, and cleaned the rest of the files.

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,19 +1,20 @@
 class MealsController < ApplicationController
-    
-    def create
-        @restaurant = Restaurant.find(params[:restaurant_id])
-        @meal = @restaurant.meals.create(meal_params)
-        redirect_to restaurant_path(@restaurant)
-    end
-    
-    def destroy
-        @restaurant = Restaurant.find(params[:restaurant_id])
-        @meal = @restaurant.meals.find(params[:id])
-        @meal.destroy
-        redirect_to restaurant_path(@restaurant)
-    end
-    
-    private def meal_params
-        params.require(:meal).permit(:name, :price)
-    end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @meal = @restaurant.meals.create(meal_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @meal = @restaurant.meals.find(params[:id])
+    @meal.destroy
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private def meal_params
+    params.require(:meal).permit(:name, :price)
+  end
+
 end
