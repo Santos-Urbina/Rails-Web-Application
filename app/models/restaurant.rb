93efdac3339a2e337446a29ee1536af5a19cba6b
Brandon Earnhardt <bmearnhardt@gmail.com>
2018-11-30 14:09:59 -0500

    Fixed issue with CSS when error appeared

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true, length: { minimum: 3 }
   validates :genre, presence: true, length: { minimum: 3 }
+  validates :rating, presence: true, length: { is: 1 }
 end
