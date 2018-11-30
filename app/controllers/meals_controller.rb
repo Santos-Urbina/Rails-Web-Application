aad5fabc0c8192a99a4ecbb3ba668cc80deceb4d
Aaron Martin <amart201@uncc.edu>
2018-11-30 01:11:21 +0000

    Add destroy functionality for meals

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,6 +6,13 @@ class MealsController < ApplicationController
         redirect_to restaurant_path(@restaurant)
     end
     
+    def destroy
+        @restaurant = Restaurant.find(params[:restaurant_id])
+        @meal = @restaurant.meals.find(params[:id])
+        @meal.destroy
+        redirect_to restaurant_path(@restaurant)
+    end
+    
     private def meal_params
         params.require(:meal).permit(:name, :price)
     end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :meals
+  has_many :meals, dependent: :destroy
   has_many :reviews, dependent: :destroy
   validates :name, presence: true, length: { minimum: 3 }
   validates :genre, presence: true, length: { minimum: 3 }
